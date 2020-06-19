98b8fd2ed3258e6422627fe8dcb572aac2ac3be9
Andrew Reynolds <andrew_reynolds@mac.com>
2020-06-19 14:33:13 -0700

    cards added to grid using javascript

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,17 @@ const projectGrid = document.querySelector('.projectGrid');
 // Functions //
 
 function createCard(item){
-
-
+    let {name, link, image, description} = item;
+    let card = document.createElement('div');
+    card.className = 'projectCard';
+    card.innerHTML = `
+        <h2>${name}</h2>
+        <div class="cardInfo">
+            <a href="${link}"><img src="${image}"></a>
+            <p>${description}</p>
+        </div>
+    `;
+    projectGrid.appendChild(card);
 }
 
 
@@ -19,26 +28,28 @@ let projectCardArray = [
     {
         name: "Tribute Page",
         link: "https://andrewrey.github.io/freecodecamp-tributePage/",
-        image: "../img/tribute.png",
+        image: "img/tribute.png",
         description: "Using HTML CSS and SASS, I created a tribute page for Chad Muska."
     },
     {
         name: "Survey Form",
         link: "https://andrewrey.github.io/freecodecamp-surveyForm/",
-        image: "../img/survey-form.png",
+        image: "img/survey-form.png",
         description: "Created a page focused on the form element. With this form I used several types of form input elements."
     },
     {
         name: "Product Landing Page",
         link: "https://andrewrey.github.io/freecodecamp-productLandingPage/",
-        image: "../img/product-landing-page.png",
+        image: "img/product-landing-page.png",
         description: "Created page using HTML CSS SASS and Javascript. This page uses several break points to be mobile first as well use JavaScript to create product section as well as overlay card of each individual product. "
     },
     {
-        name: "Technical Documentation Page",
+        name: "Tech Documentation Page",
         link: "https://andrewrey.github.io/freecodecamp-techdocumentationPage/",
-        image: "../img/technical-documentation.png",
+        image: "img/technical-documentation.png",
         description: "Page utilizes the CSS Grid and is Mobile responsive which re-organizes the grid positioning for screen size."
     },
 
 ];
+
+projectCardArray.forEach(createCard);
