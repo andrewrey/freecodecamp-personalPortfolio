32c1e86c7d74bb7ed0e24005479fa411b2688463
Andrew Reynolds <andrew_reynolds@mac.com>
2020-06-19 11:23:19 -0700

    started to add projects via javascript. Created project array for iteration with create card function

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+
+// Page Elements//
+const projectGrid = document.querySelector('.projectGrid');
+
+
+// Functions //
+
+function createCard(item){
+
+
+}
+
+
+
+
+// Card Array Objects //
+
+let projectCardArray = [
+    {
+        name: "",
+        link: "",
+        image: "",
+        description: ""
+    },
+    {
+        name: "",
+        link: "",
+        image: "",
+        description: ""
+    },
+    {
+        name: "",
+        link: "",
+        image: "",
+        description: ""
+    },
+    {
+        name: "",
+        link: "",
+        image: "",
+        description: ""
+    },
+
+];
