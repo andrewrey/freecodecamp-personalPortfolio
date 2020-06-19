8e2b5e6eb098f44dd7bbb04b5785aa4f801cac98
Andrew Reynolds <andrew_reynolds@mac.com>
2020-06-19 11:41:27 -0700

    completed project object array

--- js/scripts.js
+++ js/scripts.js
@@ -17,28 +17,28 @@ function createCard(item){
 
 let projectCardArray = [
     {
-        name: "",
-        link: "",
-        image: "",
-        description: ""
+        name: "Tribute Page",
+        link: "https://andrewrey.github.io/freecodecamp-tributePage/",
+        image: "../img/tribute.png",
+        description: "Using HTML CSS and SASS, I created a tribute page for Chad Muska."
     },
     {
-        name: "",
-        link: "",
-        image: "",
-        description: ""
+        name: "Survey Form",
+        link: "https://andrewrey.github.io/freecodecamp-surveyForm/",
+        image: "../img/survey-form.png",
+        description: "Created a page focused on the form element. With this form I used several types of form input elements."
     },
     {
-        name: "",
-        link: "",
-        image: "",
-        description: ""
+        name: "Product Landing Page",
+        link: "https://andrewrey.github.io/freecodecamp-productLandingPage/",
+        image: "../img/product-landing-page.png",
+        description: "Created page using HTML CSS SASS and Javascript. This page uses several break points to be mobile first as well use JavaScript to create product section as well as overlay card of each individual product. "
     },
     {
-        name: "",
-        link: "",
-        image: "",
-        description: ""
+        name: "Technical Documentation Page",
+        link: "https://andrewrey.github.io/freecodecamp-techdocumentationPage/",
+        image: "../img/technical-documentation.png",
+        description: "Page utilizes the CSS Grid and is Mobile responsive which re-organizes the grid positioning for screen size."
     },
 
 ];
